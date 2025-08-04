97da9e51ed124bd62f5bc711a049e4c7fff7e834
Vijay Guttula <vijayguttula001@gmail.com>
2025-08-05 01:35:20 +0530

    Added login, signup pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { AuthForm } from '@/components/auth/AuthForm';
+import { SiteHeader } from '@/components/site-header';
+
+export default function LoginPage() {
+  return (
+    <>
+      <SiteHeader showGetStarted={true}/>
+      <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-background to-black px-4 py-12">
+        <AuthForm type="login" />
+      </main>
+    </>
+  );
+}
