b1b06a8d8c3f809879dc8ca39ea74a52de5e3eca
Vijay Guttula <vijayguttula001@gmail.com>
2025-08-06 00:57:11 +0530

    Added nodejs runtime for vercek

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 // src/app/dashboard/page.tsx
+export const runtime = 'nodejs';
+
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import prisma from '@/lib/prisma';
